eb3730dfdb6df9aa0e8840a65b2521175167d226
swethasaetha-glitch <swethasaetha@gmail.com>
2026-08-21 16:24:02 +0530

    Change userName for badge update script

--- update-badge-script.js
+++ update-badge-script.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let userName = "Priyan-19";
+let userName = "swethasaetha-glitch";
 
 let pipelineRun = true; // Set to false for local debugging
 let templateFile = 'github-repo-count-template.svg';
